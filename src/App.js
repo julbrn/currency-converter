2ce414b1277aa6201903b519f3a8fd8e6d44de91
Yulia Burenko <juliaburenko@yandex.ru>
2022-10-11 20:48:33 +0300

    feat: added desktop mark-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import CurrencyRow from './CurrencyRow'
 import './App.css';
+import picture from './images/calculator.png'
 
 function App() {
   return (
-<h1>Converter</h1>
+      <div className="page">
+          <a class="credits" href="https://www.freepik.com/free-photo/calculator-money-wallet-investment-saving-finance-concept-pink-background-3d-rendering_25515805.htm#query=3d%20currency&position=34&from_view=search&track=sph">Image by mamewmy on Freepik</a>
+          <img className="picture" src={picture} alt="Money and calculator"/>
+          <div className="container">
+            <h1 className="header">Currency Converter</h1>
+            <CurrencyRow/>
+              <div>=</div>
+            <CurrencyRow/>
+          </div>
+      </div>
   );
 }
 
---
+++ src/CurrencyRow.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function CurrencyRow() {
+    return (
+        <div>
+            <input class="input" type="number"/>
+            <select class="drop-list" name="" id="">
+                <option value="MON">MON</option>
+            </select>
+        </div>
+    )
+}
